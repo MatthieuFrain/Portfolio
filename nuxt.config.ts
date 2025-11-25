88850e235ce4afa73c3903dcc3b2b78be092e627
MatthieuFrain <matthieu.frain@gmail.com>
2025-11-25 16:11:47 +0100

    feat: Update Nuxt configuration, refresh favicon, and adjust build ignore patterns, leading to new build artifacts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,13 +14,13 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@vueuse/motion/nuxt',
     '@vueuse/nuxt',
-    '@nuxtjs/sitemap',
+    // '@nuxtjs/sitemap',
   ],
 
   // Site configuration
-  site: {
-    url: 'https://www.matthieufrain.dev',
-  },
+  // site: {
+  //   url: 'https://www.matthieufrain.dev',
+  // },
 
   // Nitro configuration
   nitro: {
