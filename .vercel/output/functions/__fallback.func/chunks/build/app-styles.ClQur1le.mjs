115e61d105a0f789e3190f52b2106d93fcd0b81c
MatthieuFrain <matthieu.frain@gmail.com>
2025-11-25 16:08:40 +0100

    refactor: migrate Tailwind config to TypeScript, update dependencies, and regenerate Vercel build outputs.

--- .vercel/output/functions/__fallback.func/chunks/build/app-styles.ClQur1le.mjs
+++
@@ -1,8 +0,0 @@
-import { a as app_vue_vue_type_style_index_0_lang } from './entry-styles-4.mjs-ByAOxI3m.mjs';
-
-const appStyles_ClQur1le = [
-  app_vue_vue_type_style_index_0_lang
-];
-
-export { appStyles_ClQur1le as default };
-//# sourceMappingURL=app-styles.ClQur1le.mjs.map
